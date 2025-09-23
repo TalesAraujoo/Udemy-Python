cf407fae0fb665c70dcbd7fbd20eb3a22afd8cad
TalesAraujoo <thetimeprofessor@gmail.com>
2025-09-23 10:58:05 -0300

    Update balance_v1.py

--- Python-udemy-corse/Archive_manipulation/balance_v1.py
+++ Python-udemy-corse/Archive_manipulation/balance_v1.py
@@ -9,6 +9,7 @@ def show_menu():
     print('4. Exit\n')
     option = input('Choose an option: ')
     get_option(int(option))
+    print('\n')
 
 
 def get_option(chosen_option):
@@ -17,34 +18,59 @@ def get_option(chosen_option):
         case 1:
             add_income()
             show_menu()
+
         case 2:
             add_expense()
             show_menu()
+
         case 3: 
-            print()
+            show_report()
+            show_menu()
+            
         case 4:
             print('Exiting program...')
             print('Great job, keep shit updated!\n')
 
 
 def add_income():
-    with open('september.csv', 'a', newline='') as month: 
-        writer = csv.writer(month)
+    with open('september.csv', 'a', newline='') as csvfile: 
+        writer = csv.writer(csvfile)
         tmp_value = input('Income value: ')
         write_row = ['Income', tmp_value]
         writer.writerow(write_row)
-        month.close()
+        csvfile.close()
         print('\n')
 
 
 def add_expense():
-    with open('september.csv', 'a', newline='') as month: 
-        writer = csv.writer(month)
+    with open('september.csv', 'a', newline='') as csvfile: 
+        writer = csv.writer(csvfile)
         tmp_value = input('Expense value: ')
         write_row = ['Expense', tmp_value]
         writer.writerow(write_row)
-        month.close()
+        csvfile.close()
         print('\n')
 
 
+def show_report():
+    with open('september.csv', 'r', newline = '') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        total_income = 0
+        total_expenses = 0
+        
+        for row in csv_reader:
+            if row[0] == 'Income':
+                total_income += int(row[1])
+            else:
+                total_expenses += int(row[1])
+
+        print('------ Report ------')
+        print(f'Total income: {total_income:.2f}')
+        print(f'Total expenses: {total_expenses:.2f}')
+        print(f'Profitability: {(total_income - total_expenses):.2f}')    
+        print('\n----------------------')
+        input('Press any key to procced...')
+
+
 show_menu()
+
